28b962261468e2a837c53ac42347b3da31ca1254
Peter Molnar <peter@petermolnar.hu>
2015-11-06 21:11:28 +0100

    Initial Commit

---
+++ TextReaderHackerRank/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TextReaderHackerRank
+//
+//  Created by Peter Molnar on 06/11/2015.
+//  Copyright © 2015 Peter Molnar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
