66b4b2ac8d10cd596f51ecb57864a9ffa3046ffd
Peter Molnar <peter@petermolnar.hu>
2015-11-07 06:33:16 +0100

    Initial commit

--- TextReaderHackerRank/main.m
+++ TextReaderHackerRank/main.m
@@ -7,11 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
+int addTwoNumbers(int a, int b) {
+    return a+b;
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int testArrayLength;
+        int sumOfArray;
+        scanf("%d", &testArrayLength);
+        
+        
+        int sumArray[testArrayLength];
+        for(int i = 0; i<testArrayLength; i++){
+            int n;
+            scanf("%d",&n);
+            sumArray[i]=n;
+        }
+        sumOfArray = 0;
+        for(int j = 0; j<testArrayLength; j++){
+            sumOfArray = addTwoNumbers(sumOfArray,sumArray[j]);
+        }
+        printf("%d\n", sumOfArray);
     }
     return 0;
 }
